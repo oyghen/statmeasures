e344f065240195e9f576f4e39fab9fab697c4180
oyghen <oyghen@duck.com>
2025-12-30 11:39:49 +0100

    feat(center): add trimmed_mean

--- src/statmeasures/center.py
+++ src/statmeasures/center.py
@@ -1,9 +1,17 @@
 """Measures of central tendency."""
 
-__all__ = []
+__all__ = ("trimmed_mean", "winsorized_mean")
 
 import numpy as np
 from scipy.stats import mstats
+
+
+def trimmed_mean(
+    data: np.ndarray,
+    limits: tuple[float, float] = (0.05, 0.05),
+) -> float:
+    """Return the trimmed mean."""
+    return mstats.trimmed_mean(data, limits=limits)
 
 
 def winsorized_mean(
--- tests/test_center.py
+++ tests/test_center.py
@@ -6,6 +6,12 @@ import statmeasures as sm
 
 
 class TestMeasuresOfCentralTendency:
+    def test_trimmed_mean(self, data: np.array):
+        """Test the 20% trimmed mean."""
+        alpha = 0.2
+        result = sm.center.trimmed_mean(data, limits=(alpha, alpha))
+        npt.assert_almost_equal(result, 5.67, decimal=2)
+
     def test_winsorized_mean(self, data: np.array):
         """Test the 20% winsorized mean."""
         alpha = 0.2
