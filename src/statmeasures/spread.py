80ff7046cf53d69d6deb5a9fda5dc6830a05900f
oyghen <oyghen@duck.com>
2026-01-03 12:47:00 +0100

    refactor(spread): integrate ensure_1d

--- src/statmeasures/spread.py
+++ src/statmeasures/spread.py
@@ -4,7 +4,11 @@ __all__ = ("stderr",)
 
 import numpy as np
 
+from statmeasures.utils import Vector, ensure_1d
 
-def stderr(data: np.ndarray) -> float:
+
+def stderr(vec: Vector, /, *, validate: bool = False) -> float:
     """Return the standard error."""
-    return data.std(ddof=1) / np.sqrt(len(data))
+    v = ensure_1d(vec) if validate else vec
+    res = v.std(ddof=1) / np.sqrt(len(v))
+    return float(res)
--- tests/test_spread.py
+++ tests/test_spread.py
@@ -3,13 +3,14 @@ import numpy.testing as npt
 import pytest
 
 import statmeasures as sm
+from statmeasures.utils import Vector
 
 
 class TestMeasuresOfDispersion:
-    def test_stderr(self, data: np.array):
-        result = sm.spread.stderr(data)
+    def test_stderr(self, vec: Vector):
+        result = sm.spread.stderr(vec)
         npt.assert_almost_equal(result, 5.9632, decimal=4)
 
     @pytest.fixture
-    def data(self) -> np.array:
+    def vec(self) -> Vector:
         return np.array([98, 127, 82, 67, 121, 119, 92, 110, 113, 107])
